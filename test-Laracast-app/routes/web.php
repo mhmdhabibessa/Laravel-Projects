75ff764b6e31df328f12c665ec9623cb10329277
mhmdhabibessa <mhmdhabib1999@gmail.com>
2021-11-13 16:09:51 +0200

    all

--- test-Laracast-app/routes/web.php
+++ test-Laracast-app/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
+});
+
+
+Route::get('posts/{post}',function($id){
+    return view('post',[
+        'post' => Post::findOrFail($id)
+    ]);
 });
